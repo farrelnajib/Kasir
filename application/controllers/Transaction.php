4deea38e3ba0938dd2ccc7ee7aa765908879471e
Farrel Anshary <farrelnajib@gmail.com>
2020-06-02 21:07:35 +0700

    Change SMTP settings and fix invoice display

--- application/controllers/Transaction.php
+++ application/controllers/Transaction.php
@@ -3,150 +3,148 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Transaction extends CI_Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
+	public function __construct()
+	{
+		parent::__construct();
 
-        date_default_timezone_set('Asia/Jakarta');
-        $this->load->model('Transaction_model');
-        $this->load->model('Category_model');
-        $this->load->model('Order_model');
-        $this->load->model('Payment_model');
-        $this->load->model('Payment_method_model');
-        $this->load->model('Menu_model');
+		date_default_timezone_set('Asia/Jakarta');
+		$this->load->model('Transaction_model');
+		$this->load->model('Category_model');
+		$this->load->model('Order_model');
+		$this->load->model('Payment_model');
+		$this->load->model('Payment_method_model');
+		$this->load->model('Menu_model');
 
-        if (empty($this->session->userdata('id')) || $this->session->userdata('isLoggedIn') != true) {
-            $this->session->set_flashdata('danger', 'Please log in first');
-            redirect(base_url('login'));
-        }
+		if (empty($this->session->userdata('id')) || $this->session->userdata('isLoggedIn') != true) {
+			$this->session->set_flashdata('danger', 'Please log in first');
+			redirect(base_url('login'));
+		}
 
-        if ($this->uri->segment(1) == 'order' || ($this->uri->segment(1) == 'transaction' && $this->uri->segment(2) == '')) {
-            redirect(base_url());
-        }
-    }
-
-
-    public function index()
-    {
-        $data['transactions'] = $this->Transaction_model->getAll();
-        $data['unfinished_transactions'] = $this->Transaction_model->getUnfinished();
-        $this->load->view('index', $data);
-    }
+		if ($this->uri->segment(1) == 'order' || ($this->uri->segment(1) == 'transaction' && $this->uri->segment(2) == '')) {
+			redirect(base_url());
+		}
+	}
 
 
-    public function newTransaction()
-    {
-        $id = date("YmdHis");
-        $date = date("Y-m-d H:i:s");
+	public function index()
+	{
+		$data['transactions'] = $this->Transaction_model->getAll();
+		$data['unfinished_transactions'] = $this->Transaction_model->getUnfinished();
+		$this->load->view('index', $data);
+	}
 
-        $existingID = $this->Transaction_model->getUnique($id);
-        if (count($existingID) > 0) {
-            sleep(1);
-            $this->newTransaction();
-        }
 
-        $dataTransaction = [
-            'transaction_id' => $id,
-            'transaction_total' => 0,
-            'transaction_payment' => 0,
-            'transaction_change' => 0,
-            'transaction_open_bill' => $date
-        ];
+	public function newTransaction()
+	{
+		$id = date("YmdHis");
+		$date = date("Y-m-d H:i:s");
 
-        if ($this->Transaction_model->insert($dataTransaction)) {
-            $smallestPaymentMethod = $this->Payment_method_model->getSmallestId()[0]->method_id;
-            $dataPayment = [
-                'transaction_id' => $id,
-                'method_id' => $smallestPaymentMethod
-            ];
-            if ($this->Payment_model->insert($dataPayment)) {
-                redirect(base_url('order/') . $id);
-            }
-        }
-    }
+		$existingID = $this->Transaction_model->getUnique($id);
+		if (count($existingID) > 0) {
+			sleep(1);
+			$this->newTransaction();
+		}
 
-    public function finishTransaction($id)
-    {
-        $closeBill = date("Y-m-d H:i:s");
+		$dataTransaction = [
+			'transaction_id' => $id,
+			'transaction_total' => 0,
+			'transaction_payment' => 0,
+			'transaction_change' => 0,
+			'transaction_open_bill' => $date
+		];
 
-        if ($this->Transaction_model->update($id, ['transaction_close_bill' => $closeBill])) {
-            if ($this->invoice($id)) {
-                $this->session->set_flashdata('success', 'Success close bill');
-                redirect(base_url());
-            } else {
-                $this->session->set_flashdata('danger', 'Failed to send bill');
-                redirect(base_url());
-            }
-        } else {
-            $this->session->set_flashdata('danger', 'Failed close bill');
-            redirect(base_url());
-        }
-    }
+		if ($this->Transaction_model->insert($dataTransaction)) {
+			$smallestPaymentMethod = $this->Payment_method_model->getSmallestId()[0]->method_id;
+			$dataPayment = [
+				'transaction_id' => $id,
+				'method_id' => $smallestPaymentMethod
+			];
+			if ($this->Payment_model->insert($dataPayment)) {
+				redirect(base_url('order/') . $id);
+			}
+		}
+	}
 
-    public function delete($id)
-    {
-        if ($this->Payment_model->deleteTransaction($id)) {
-            if ($this->Order_model->deleteTransaction($id)) {
-                if ($this->Transaction_model->delete($id)) {
-                    $this->session->set_flashdata('success', 'Successfully deleted transaction');
-                    redirect(base_url());
-                } else {
-                    $this->session->set_flashdata('danger', 'Failed delete transaction');
-                    redirect(base_url());
-                }
-            } else {
-                $this->session->set_flashdata('danger', 'Failed delete orders');
-                redirect(base_url());
-            }
-        } else {
-            $this->session->set_flashdata('danger', 'Failed delete payments');
-            redirect(base_url());
-        }
-    }
+	public function finishTransaction($id)
+	{
+		$closeBill = date("Y-m-d H:i:s");
 
-    public function invoice($id)
-    {
-        $data['transaction'] = $this->Transaction_model->getById($id)[0];
-        $data['orders'] = $this->Order_model->getOrders($id);
-        $data['payments'] = $this->Payment_model->getByTransactionId($id);
+		if ($this->Transaction_model->update($id, ['transaction_close_bill' => $closeBill])) {
+			if ($this->invoice($id)) {
+				$this->session->set_flashdata('success', 'Success close bill');
+				redirect(base_url());
+			} else {
+				$this->session->set_flashdata('danger', 'Failed to send bill');
+				redirect(base_url());
+			}
+		} else {
+			$this->session->set_flashdata('danger', 'Failed close bill');
+			redirect(base_url());
+		}
+	}
 
-        $data['transaction']->transaction_subtotal = $this->Order_model->getSubtotal($id)[0]->order_subtotal;
-        $data['transaction']->transaction_tax = $data['transaction']->transaction_subtotal * 0.1;
-        $data['transaction']->transaction_open_bill = date("l, d F Y H:i:s", strtotime($data['transaction']->transaction_open_bill));
-        $data['transaction']->transaction_close_bill = date("l, d F Y H:i:s", strtotime($data['transaction']->transaction_close_bill));
+	public function delete($id)
+	{
+		if ($this->Payment_model->deleteTransaction($id)) {
+			if ($this->Order_model->deleteTransaction($id)) {
+				if ($this->Transaction_model->delete($id)) {
+					$this->session->set_flashdata('success', 'Successfully deleted transaction');
+					redirect(base_url());
+				} else {
+					$this->session->set_flashdata('danger', 'Failed delete transaction');
+					redirect(base_url());
+				}
+			} else {
+				$this->session->set_flashdata('danger', 'Failed delete orders');
+				redirect(base_url());
+			}
+		} else {
+			$this->session->set_flashdata('danger', 'Failed delete payments');
+			redirect(base_url());
+		}
+	}
 
-        $config = [
-            'protocol' => 'smtp',
-            'smtp_host' => 'ssl://smtp.gmail.com',
-            'smtp_user' => 'waroenkabnormal@gmail.com',
-            'smtp_pass' => 'anshary08',
-            'smtp_port' => '465',
-            'mailtype' => 'html',
-            'charset' => 'utf-8',
-            'newline' => "\r\n",
-        ];
-        $this->load->library('email');
-        $this->email->initialize($config);
+	public function invoice($id)
+	{
+		$data['transaction'] = $this->Transaction_model->getById($id)[0];
+		$data['orders'] = $this->Order_model->getOrders($id);
+		$data['payments'] = $this->Payment_model->getByTransactionId($id);
 
-        $this->email->from('waroenkabnormal@gmail.com', 'Waroenk Abnormal');
-        $this->email->to($data['transaction']->customer_email);
-        $this->email->subject('Your E-receipt for transaction ' . $id);
-        $this->email->message($this->load->view('Invoice', $data, true));
+		$data['transaction']->transaction_subtotal = $this->Order_model->getSubtotal($id)[0]->order_subtotal;
+		$data['transaction']->transaction_tax = $data['transaction']->transaction_subtotal * 0.1;
+		$data['transaction']->transaction_open_bill = date("l, d F Y H:i:s", strtotime($data['transaction']->transaction_open_bill));
+		$data['transaction']->transaction_close_bill = date("l, d F Y H:i:s", strtotime($data['transaction']->transaction_close_bill));
 
-        $this->email->send();
+		$config = [
+			'protocol' => 'smtp',
+			'smtp_host' => 'mail.smtp2go.com',
+			'smtp_port' => 2525,
+			'smtp_user' => 'waroenkabnormal',
+			'smtp_pass' => 'anshary08',
+			'mailtype' => 'html',
+			'charset' => 'utf-8',
+			'newline' => "\r\n",
+		];
+		$this->load->library('email');
+		$this->email->initialize($config);
 
-        if ($this->email->send()) {
-            if ($this->input->get('method') == 'resend') {
-                echo json_encode(['status' => true]);
-            } else {
-                return true;
-            }
-        } else {
-            if ($this->input->get('method') == 'resend') {
-                echo json_encode(['status' => false]);
-            } else {
-                return false;
-            }
-        }
-    }
+		$this->email->from('farrel.anshary@binus.ac.id', 'Waroenk Abnormal');
+		$this->email->to($data['transaction']->customer_email);
+		$this->email->subject('Your E-receipt for transaction ' . $id);
+		$this->email->message($this->load->view('Invoice', $data, true));
+
+		if ($this->email->send()) {
+			if ($this->input->get('method') == 'resend') {
+				echo json_encode(['status' => true]);
+			} else {
+				return true;
+			}
+		} else {
+			if ($this->input->get('method') == 'resend') {
+				echo json_encode(['status' => false]);
+			} else {
+				return false;
+			}
+		}
+	}
 }
--- application/views/Invoice.php
+++ application/views/Invoice.php
@@ -1,7 +1,7 @@
 <?php
 $email = $transaction->customer_email;
 if (empty($transaction->customer_email)) {
-  $email = $transaction->customer_phone;
+	$email = $transaction->customer_phone;
 }
 ?>
 
@@ -9,128 +9,219 @@ if (empty($transaction->customer_email)) {
 <html>
 
 <head>
-  <meta charset="utf-8">
-  <link rel="stylesheet" href="<?= base_url('assets/css/invoice.css'); ?>">
+	<meta charset="utf-8">
+	<style>
+		.invoice-box {
+			max-width: 800px;
+			margin: auto;
+			padding: 30px;
+			border: 1px solid #eee;
+			box-shadow: 0 0 10px rgba(0, 0, 0, .15);
+			font-size: 16px;
+			line-height: 24px;
+			font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+			color: #555;
+		}
+
+		.invoice-box table {
+			width: 100%;
+			line-height: inherit;
+		}
+
+		.invoice-box table td {
+			padding: 5px;
+			vertical-align: top;
+		}
+
+		.invoice-box table tr.top table td {
+			padding-bottom: 20px;
+		}
+
+		.invoice-box table tr.top table td.title {
+			font-size: 45px;
+			line-height: 45px;
+			color: #333;
+		}
+
+		.invoice-box table tr.information table td {
+			padding-bottom: 40px;
+		}
+
+		.invoice-box table tr.heading td {
+			background: #eee;
+			border-bottom: 1px solid #ddd;
+			font-weight: bold;
+		}
+
+		.invoice-box table tr.item td {
+			border-bottom: 1px solid #eee;
+		}
+
+		.invoice-box table tr.item.last td {
+			border-bottom: none;
+		}
+
+		.invoice-box table tr.total td:nth-child(4) {
+			border-top: 2px solid #eee;
+			font-weight: bold;
+		}
+
+		td:nth-child(4) {
+			text-align: right;
+		}
+
+		@media only screen and (max-width: 600px) {
+			.invoice-box table tr.top table td {
+				width: 100%;
+				display: block;
+				text-align: center;
+			}
+
+			.invoice-box table tr.information table td {
+				width: 100%;
+				display: block;
+				text-align: center;
+			}
+		}
+
+		/** RTL **/
+		.rtl {
+			direction: rtl;
+			font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+		}
+
+		.rtl table {
+			text-align: right;
+		}
+
+		.rtl table tr td:nth-child(2) {
+			text-align: left;
+		}
+
+		.jumlah {
+			text-align: center
+		}
+	</style>
 </head>
 
 <body>
-  <div class="invoice-box">
-    <table cellpadding="0" cellspacing="0">
-      <tr class="top">
-        <td colspan="4">
-          <table>
-            <td class="title">
-              <center>
-                Waroenk Abnormal
-              </center>
-            </td>
+	<div class="invoice-box">
+		<table cellpadding="0" cellspacing="0">
+			<tr class="top">
+				<td colspan="4">
+					<table>
+						<td class="title">
+							<center>
+								Waroenk Abnormal
+							</center>
+						</td>
 
-            <tr>
-              <td>
-                Invoice : <?= $transaction->transaction_id; ?> <br>
-                Open bill: <?= $transaction->transaction_open_bill; ?><br>
-                Close bill: <?= $transaction->transaction_close_bill; ?><br>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
+						<tr>
+							<td>
+								Invoice : <?= $transaction->transaction_id; ?> <br>
+								Open bill: <?= $transaction->transaction_open_bill; ?><br>
+								Close bill: <?= $transaction->transaction_close_bill; ?><br>
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
 
-      <tr class="information">
-        <td colspan="4">
-          <table>
-            <tr>
-              <td>
-                Jalan Pasir Kaliki No.25-27 <br>
-                Kebon Jeruk, Andir, Bandung, 40181 <br>
-                (022) 20568888
-              </td>
+			<tr class="information">
+				<td colspan="4">
+					<table>
+						<tr>
+							<td>
+								Jalan Pasir Kaliki No.25-27 <br>
+								Kebon Jeruk, Andir, Bandung, 40181 <br>
+								(022) 20568888
+							</td>
 
-              <td style="text-align: right">
-                Customer: <br>
-                <?= $transaction->customer_name; ?><br>
-                <?= $email; ?>
-              </td>
-            </tr>
-          </table>
-        </td>
-      </tr>
+							<td style="text-align: right">
+								Customer: <br>
+								<?= $transaction->customer_name; ?><br>
+								<?= $email; ?>
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>
 
-      <tr class="heading">
-        <td>Item</td>
-        <td>Harga</td>
-        <td class="jumlah">Jumlah</td>
-        <td>Subtotal</td>
-      </tr>
+			<tr class="heading">
+				<td>Item</td>
+				<td>Harga</td>
+				<td class="jumlah">Jumlah</td>
+				<td>Subtotal</td>
+			</tr>
 
-      <?php foreach ($orders as $order) : ?>
-        <tr class="details">
-          <td><?= $order->menu_name; ?></td>
-          <td>Rp. <?= number_format($order->menu_final_price); ?></td>
-          <td class="jumlah"><?= $order->order_quantity; ?></td>
-          <td>Rp. <?= number_format($order->order_subtotal); ?></td>
-        </tr>
-      <?php endforeach; ?>
+			<?php foreach ($orders as $order) : ?>
+				<tr class="details">
+					<td><?= $order->menu_name; ?></td>
+					<td>Rp. <?= number_format($order->menu_final_price); ?></td>
+					<td class="jumlah"><?= $order->order_quantity; ?></td>
+					<td>Rp. <?= number_format($order->order_subtotal); ?></td>
+				</tr>
+			<?php endforeach; ?>
 
-      <tr class="heading">
-        <td>Totals</td>
-        <td></td>
-        <td></td>
-        <td>Jumlah</td>
-      </tr>
+			<tr class="heading">
+				<td>Totals</td>
+				<td></td>
+				<td></td>
+				<td>Jumlah</td>
+			</tr>
 
-      <tr class="item">
-        <td>Subtotal</td>
-        <td></td>
-        <td></td>
-        <td>Rp. <?= number_format($transaction->transaction_subtotal); ?></td>
-      </tr>
+			<tr class="item">
+				<td>Subtotal</td>
+				<td></td>
+				<td></td>
+				<td>Rp. <?= number_format($transaction->transaction_subtotal); ?></td>
+			</tr>
 
-      <tr class="item">
-        <td>Tax</td>
-        <td></td>
-        <td></td>
-        <td>Rp. <?= number_format($transaction->transaction_tax); ?></td>
-      </tr>
+			<tr class="item">
+				<td>Tax</td>
+				<td></td>
+				<td></td>
+				<td>Rp. <?= number_format($transaction->transaction_tax); ?></td>
+			</tr>
 
-      <tr class="item">
-        <td>Total</td>
-        <td></td>
-        <td></td>
-        <td>Rp. <?= number_format($transaction->transaction_total); ?></td>
-      </tr>
+			<tr class="item">
+				<td>Total</td>
+				<td></td>
+				<td></td>
+				<td>Rp. <?= number_format($transaction->transaction_total); ?></td>
+			</tr>
 
-      <tr class="heading">
-        <td>Pembayaran</td>
-        <td></td>
-        <td></td>
-        <td>Jumlah</td>
-      </tr>
+			<tr class="heading">
+				<td>Pembayaran</td>
+				<td></td>
+				<td></td>
+				<td>Jumlah</td>
+			</tr>
 
-      <?php foreach ($payments as $payment) : ?>
-        <tr class="item">
-          <td>Pembayaran: <?= $payment->method_name; ?></td>
-          <td></td>
-          <td></td>
-          <td>Rp. <?= number_format($payment->payment_amount); ?></td>
-        </tr>
-      <?php endforeach; ?>
+			<?php foreach ($payments as $payment) : ?>
+				<tr class="item">
+					<td>Pembayaran: <?= $payment->method_name; ?></td>
+					<td></td>
+					<td></td>
+					<td>Rp. <?= number_format($payment->payment_amount); ?></td>
+				</tr>
+			<?php endforeach; ?>
 
-      <tr class="item">
-        <td>Total pembayaran</td>
-        <td></td>
-        <td></td>
-        <td>Rp. <?= number_format($transaction->transaction_payment); ?></td>
-      </tr>
+			<tr class="item">
+				<td>Total pembayaran</td>
+				<td></td>
+				<td></td>
+				<td>Rp. <?= number_format($transaction->transaction_payment); ?></td>
+			</tr>
 
-      <tr class="total">
-        <td>Kembalian</td>
-        <td></td>
-        <td></td>
-        <td>Rp. <?= number_format($transaction->transaction_change); ?></td>
-      </tr>
-    </table>
-  </div>
+			<tr class="total">
+				<td>Kembalian</td>
+				<td></td>
+				<td></td>
+				<td>Rp. <?= number_format($transaction->transaction_change); ?></td>
+			</tr>
+		</table>
+	</div>
 </body>
 
 </html>
